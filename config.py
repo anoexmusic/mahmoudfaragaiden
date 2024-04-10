b0e486d5286c9fde7f2f6f0d03b38f13a2f2cb75
anoexmusic <150429754+anoexmusic@users.noreply.github.com>
2024-04-10 23:18:29 +0200

    Add files via upload

--- config.py
+++ config.py
@@ -83,8 +83,8 @@ START_IMG_URL = getenv(
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://te.legra.ph/file/b8a0c1a00db3e57522b53.jpg"
 )
-PLAYLIST_IMG_URL = "https://telegra.ph/file/788277230299a413ae022.jpg"
-STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
+PLAYLIST_IMG_URL = "https://telegra.ph/file/4268ffbc7c67adae859f6.jpg"
+STATS_IMG_URL = "https://telegra.ph/file/4268ffbc7c67adae859f6.jpg"
 TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
 TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
 STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
