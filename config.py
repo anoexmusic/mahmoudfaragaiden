164027a86cf53f6c98b581b63bcf87c64cb5c9f9
anoexmusic <150429754+anoexmusic@users.noreply.github.com>
2024-04-14 19:43:14 +0200

    Update config.py

--- config.py
+++ config.py
@@ -78,12 +78,12 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/6d5ba44c6f4c607018ce7.jpg"
+    "START_IMG_URL", "https://telegra.ph/file/7057c15dd166e40512e8b.jpg"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://te.legra.ph/file/b8a0c1a00db3e57522b53.jpg"
 )
-PLAYLIST_IMG_URL = "https://telegra.ph/file/6d5ba44c6f4c607018ce7.jpg"
+PLAYLIST_IMG_URL = "https://telegra.ph/file/7057c15dd166e40512e8b.jpg"
 STATS_IMG_URL = "https://telegra.ph/file/4268ffbc7c67adae859f6.jpg"
 TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
 TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
